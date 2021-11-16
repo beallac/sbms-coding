6f052f5db519741dacbeabed6ec4b60d03bfe00f
Andy B <you@example.com>
2021-11-16 13:54:41 -0800

    Added compass and formatted sensor output in webots

--- bots/bot1/controllers/motors_only/motors_only.py
+++ bots/bot1/controllers/motors_only/motors_only.py
@@ -4,6 +4,18 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot, Motor, DistanceSensor
 
+from controller import Supervisor
+import sys, math
+supervisor = Supervisor()
+# do this once only
+robot_node = supervisor.getFromDef("MY_ROBOT")
+if robot_node is None:
+    sys.stderr.write("No DEF MY_ROBOT node found in the current world file\n")
+    sys.exit(1)
+else:
+    print("Supervisor activated!!!")
+trans_field = robot_node.getField("translation")
+
 import socket, select
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,7 +24,8 @@ server_address = ('localhost', 10020)
 server_socket.bind(server_address)
 
 # create the Robot instance.
-robot = Robot()
+# robot = Robot()
+robot = supervisor
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
@@ -72,16 +85,23 @@ rightMotor.setVelocity(0)
 
 server_socket.listen(1)
 
-
-
 read_list = [server_socket]
 
 while robot.step(TIME_STEP) != -1:
 
+    # # print(robot_node.getPosition())
+    R = robot_node.getOrientation()
+    # # y = (m02 - m20)/√((m21 - m12)2+(m02 - m20)2+(m10 - m01)2)
+    y = (R[2]-R[6]) / math.sqrt((R[7]-R[5])**2 + (R[2]-R[6])**2 + (R[3]-R[1])**2)
+    # # angle = acos(( m00 + m11 + m22 - 1)/2)
+    angle = math.acos(( R[0] + R[4] + R[8] - 1)/2) / math.pi * 180
+    angle *= -y
+    
     m = ""
     for sensor in sensors:
         reading = sensor.getValue()
-        m += ( "{:.2f} ".format(reading) )
+        m += ( "{:6.1f} ".format(reading) )
+    m += ( "{:6.1f} ".format(angle) )
     print(m)
     # print('d: ', d0.getValue(), d1.getValue(), d2.getValue(), d3.getValue(), d4.getValue(), d5.getValue(), d6.getValue(), d7.getValue() )
 
@@ -94,11 +114,11 @@ while robot.step(TIME_STEP) != -1:
         else:
             data = s.recv(1024)
             if data:
-                print(data)
+                # print(data)
                 sp = data.split()
 
                 if sp[0] == b'D' and len(sp) == 3:
-                    print('setting speed')
+                    # print('setting speed')
 
                     #leftMotor.setVelocity(0.1 * MAX_SPEED)
                     #rightMotor.setVelocity(0.1 * MAX_SPEED)
@@ -122,8 +142,15 @@ while robot.step(TIME_STEP) != -1:
                         m += ( "{:.2f} ".format(reading) )
                         b = bytes(m, 'utf-8')
                     s.send(b)
+                
+                if sp[0] == b'C':
+                
+                    m = 'C '                    
+                    m += ( "{:.2f} ".format(angle) )
+                    b = bytes(m, 'utf-8')
+                    s.send(b)
                   
-                    
+                                    
             else:
                 s.close()
                 read_list.remove(s)
--- sbots.py
+++ sbots.py
@@ -34,6 +34,24 @@ def pollSensors():
 
     return [ float(s[1]), float(s[2]), float(s[3]), float(s[4]), float(s[5]), float(s[6]), float(s[7]) ]
 
+def pollCompass():
+    m = "C "
+    b = bytes(m, 'utf-8')
+    sock.sendall(b)
+
+    # Add sleep here
+    time.sleep(0.05)
+
+    d = sock.recv(1024)
+    # print("Data", d)
+    
+    s = d.split()
+    # print('len', len(s))
+    if len(s) != 2:
+        return 0
+
+    return float(s[1])
+
 
 if __name__ == "__main__":
 
