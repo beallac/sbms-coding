8dd1c76207b29c5f725ca59324a05484458d0d82
Andy B <you@example.com>
2022-02-14 13:55:07 -0800

    embedded Explosoin class into sbms_game module

--- sbms_game.py
+++ sbms_game.py
@@ -5,9 +5,6 @@ class Grid():
 
         self.maze = arcade.ShapeElementList()
 
-        # x_inc = arcade.get_display_size()[0] / grid_size
-        # y_inc = arcade.get_display_size()[0] / grid_size
-
         x_inc = arcade.get_window().width / grid_size
         y_inc = arcade.get_window().height / grid_size
 
@@ -42,10 +39,71 @@ class Grid():
             x = 0 + x_inc / 2
             y -= y_inc
 
-        # self.maze = arcade.ShapeElementList()
 
-        # for i in range(10):
-        #     s = arcade.create_rectangle_filled(i*10, i*10, 10, 10, arcade.csscolor.BLACK)
-        #     self.maze.append(s)
 
-        
+EXPLOSION_TEXTURE_COUNT = 60
+
+class Explosion(arcade.Sprite):
+    # Variables that will hold sprite lists
+    explosions_list = None
+
+    # Pre-load the animation frames. We don't do this in the __init__
+    # of the explosion sprite because it
+    # takes too long and would cause the game to pause.
+    explosion_texture_list = []
+
+    columns = 16
+    count = 60
+    sprite_width = 256
+    sprite_height = 256
+    file_name = ":resources:images/spritesheets/explosion.png"
+
+    # Load the explosions from a sprite sheet
+    explosion_texture_list = arcade.load_spritesheet(file_name, sprite_width, sprite_height, columns, count)
+
+    # Load sounds. Sounds from kenney.nl
+    hit_sound = arcade.sound.load_sound(":resources:sounds/explosion2.wav")
+
+    explosions_list = arcade.SpriteList()
+
+    @classmethod
+    def draw(cls):
+        cls.explosions_list.update()
+        cls.explosions_list.draw()
+    
+    @classmethod
+    def hit(cls, location):
+        # Make an explosion
+        explosion = Explosion(cls.explosion_texture_list)
+
+        # Move it to the location of the coin
+        explosion.center_x = location[0]
+        explosion.center_y = location[1]
+
+        # Call update() because it sets which image we start on
+        explosion.update()
+
+        # Add to a list of sprites that are explosions
+        cls.explosions_list.append(explosion)
+
+        # Hit Sound
+        arcade.sound.play_sound(cls.hit_sound)
+
+    def __init__(self, texture_list):
+        super().__init__()
+
+        # Start at the first frame
+        self.current_texture = 0
+        self.textures = texture_list
+
+    def update(self):
+
+        # Update to the next frame of the animation. If we are at the end
+        # of our frames, then delete this sprite.
+        self.current_texture += 1
+        if self.current_texture < len(self.textures):
+            self.set_texture(self.current_texture)
+        else:
+            self.remove_from_sprite_lists()
+
+
